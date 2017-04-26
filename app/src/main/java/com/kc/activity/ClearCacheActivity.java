3e18840309c646936aa7fc8d693952a501e31069
Peter <successfulpeter@163.com>
2017-04-27 00:49:11 +0800

    增加清空缓存入口

---
+++ app/src/main/java/com/kc/activity/ClearCacheActivity.java
@@ -0,0 +1,15 @@
+package com.kc.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kc.label.R;
+
+public class ClearCacheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clear_cache);
+    }
+}
--- app/src/main/java/com/kc/fragment/HomeSettingFragment.java
+++ app/src/main/java/com/kc/fragment/HomeSettingFragment.java
@@ -1,6 +1,7 @@
 package com.kc.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -8,9 +9,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.kc.activity.ClearCacheActivity;
 import com.kc.adapter.CommonAdapter;
 import com.kc.adapter.ViewHolder;
 import com.kc.base.BaseFragment;
@@ -23,10 +26,11 @@ import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeSettingFragment extends BaseFragment {
+public class HomeSettingFragment extends BaseFragment implements View.OnClickListener {
 
     private Spinner mSpinnerSelectDb;
     private TextView mTvTip;
+    private LinearLayout mLLayoutClear;
 
     public HomeSettingFragment() {
         // Required empty public constructor
@@ -81,6 +85,17 @@ public class HomeSettingFragment extends BaseFragment {
     protected void findViews(View view) {
         mSpinnerSelectDb = (Spinner) view.findViewById(R.id.spinner_select_db);
         mTvTip = (TextView) view.findViewById(R.id.tv_tip);
+        mLLayoutClear = getViewById(view, R.id.llayout_setting_clear);
+
+        mLLayoutClear.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.llayout_setting_clear:
+                startActivity(new Intent(getActivity(), ClearCacheActivity.class));
+                break;
+        }
+    }
 }
